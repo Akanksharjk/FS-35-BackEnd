316c5cffa36983dd5daf26ff11b4456b4c816483
Aakanskha Rajak <aakanksharajak9@gmail.com>
2026-07-22 18:42:28 +0530

    qwer

--- instagram_backend-main/src/controllers/user.controller.js
+++ instagram_backend-main/src/controllers/user.controller.js
@@ -1,23 +1,84 @@
 import userModel from "../models/user.model.js"
 
-export const getMe = async(req, res)=>{
+export const getMe = async (req, res) => {
     try {
         const user = await userModel.findById(req.user.id).select('-password')
 
-        if(!user)return res.status(404).json({
-            success:false,
-            message:" user not found"
+        if (!user) return res.status(404).json({
+            success: false,
+            message: " user not found"
         })
         return res.status(200).json({
-            success:true,
-            message:"User found successfully",
+            success: true,
+            message: "User found successfully",
             user
         })
     } catch (error) {
         return res.status(500).json({
-            success:false,
-            message:"Internal server error"
+            success: false,
+            message: "Internal server error"
         })
     }
 }
+
+export const updateProfile = async (req, res) => {
+    try {
+        const { username, fullName, mobile, dob, bio } = req.body
+
+        const updateData = {}
+
+        if (!username || !fullName || !mobile || !dob || !bio) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+        const updatedUser = await userModel.findByIdAndUpdate(req.user.id, updateData, {
+            new: true
+        })
+
+        if (!updatedUser) return res.status(400).json({
+            success: false,
+            message: "user not found",
+        })
+
+        return res.status(200).json({
+            success: true,
+            message: "User profile updated successfully",
+            updateData
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+    }
+}
+
+export const getUserProfile = async (req, res) => {
+    try {
+        const { username } = req.params
+
+        const user = await userModel.findOne({ username }).select("-password")
+        if (!user) return res.status(400).json({
+            success: false,
+            message: "user not found"
+        })
+
+        return res.status(200).json({
+            success: true,
+            message: "user found successfully",
+            user
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "internal server error",
+            error: error.message
+        })
+    }
+}
+
+
 
--- instagram_backend-main/src/routes/user.routes.js
+++ instagram_backend-main/src/routes/user.routes.js
@@ -1,5 +1,11 @@
 import express from 'express'
+import { getMe, getUserProfile, updateProfile } from '../controllers/user.controller.js'
+import { authMiddleware } from '../middlewares/auth.middlewares.js'
 
 const routes = express.Router()
+
+routes.get('/getMe', authMiddleware, getMe)
+routes.patch('/update-profile', authMiddleware, updateProfile)
+routes.get('/:username', authMiddleware, getUserProfile)
 
 export default routes
